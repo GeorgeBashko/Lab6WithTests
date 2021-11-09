457d21df2bda75e4b0d912d8562f77ea5fde5c56
geo <grgbashko@gmail.com>
2021-11-09 19:09:51 +0300

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/Google_Tests_run.dir/DependInfo.cmake
@@ -0,0 +1,33 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "F:/untitled5/drob.cpp" "F:/untitled5/cmake-build-debug/CMakeFiles/Google_Tests_run.dir/drob.cpp.obj"
+  "F:/untitled5/test1.cpp" "F:/untitled5/cmake-build-debug/CMakeFiles/Google_Tests_run.dir/test1.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "MSVC")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../googletest/googletest/include"
+  "../googletest/googletest"
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "F:/untitled5/cmake-build-debug/googletest/googletest/CMakeFiles/gtest.dir/DependInfo.cmake"
+  "F:/untitled5/cmake-build-debug/googletest/googletest/CMakeFiles/gtest_main.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
